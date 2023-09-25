6ab0623b5ad980ff1e3ea4153178526777d98b25
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-09-25 22:31:36 +0800

    update google analytics

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -47,5 +47,15 @@
 
     @stack('reg-script')
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FL2B81V05J"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-FL2B81V05J');
+    </script>
+
 </body>
 </html>
