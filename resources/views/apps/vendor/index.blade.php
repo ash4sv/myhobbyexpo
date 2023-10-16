ad0f6734cc612ec875e8920207e763bfb84ae8a1
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-10-16 15:22:51 +0800

    update

--- resources/views/apps/vendor/index.blade.php
+++ resources/views/apps/vendor/index.blade.php
@@ -69,7 +69,7 @@
                     </td>
                     <td>
                         @foreach($vendor->registerBooth as $register)
-                            {{ $register->booth_number }} @if (!$loop->last), @endif
+                            {{ $register->booth_number }}@if (!$loop->last), @endif
                         @endforeach
                     </td>
                     <td nowrap="">
