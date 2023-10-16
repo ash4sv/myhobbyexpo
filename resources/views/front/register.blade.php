968582e26d37e1a669f22a9efb34d305308f6cc1
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-10-16 10:18:58 +0800

    update form 10 to 15 qty number

--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -138,6 +138,11 @@
                                                             <option value="8">8</option>
                                                             <option value="9">9</option>
                                                             <option value="10">10</option>
+                                                            <option value="11">11</option>
+                                                            <option value="12">12</option>
+                                                            <option value="13">13</option>
+                                                            <option value="14">14</option>
+                                                            <option value="15">15</option>
                                                         </select>
                                                         <input type="text" aria-label="" class="form-control" name="booth_price" id="booth_price" readonly value="RM ">
                                                         <input type="hidden" name="booth_price_unit" id="booth_price_unit" value="">
