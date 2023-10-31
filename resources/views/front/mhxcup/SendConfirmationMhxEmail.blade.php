979b724c832db2fee2ef6660d5e5f95fe514eb62
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-01 02:02:33 +0800

    update email

--- resources/views/front/mhxcup/SendConfirmationMhxEmail.blade.php
+++ resources/views/front/mhxcup/SendConfirmationMhxEmail.blade.php
@@ -1,5 +1,4 @@
 <x-mail::message>
-# Confirmation Register
 
 Dear {{ $full_name }},
 
@@ -7,16 +6,17 @@ Thank you for registering for the Mini 4WD MHX 2023 racer event. We are excited 
 
 You have registered for the <span style="text-transform: capitalize;">{{ $category }}</span>. Below are your registration details.
 
-Ref: {{ $uniq }}
-Full Name: {{ $full_name }}
-Identification Card Number: {{ $identification_card_number }}
-Group: {{ $group }}
-Email: {{ $email }}
-Phone Number: {{ $phone_number }}
-Category: {{ $category }}
-Registration slot: {{ $registration }}
-Nickname: {{ $nickname }}
-Race ID: @foreach($runNum as $number) {{ $nickname }}{{ $number->register }} @if (!$loop->last), @endif @endforeach
+    Ref: {{ $uniq }}
+    Full Name: {{ $full_name }}
+    Identification Card Number: {{ $identification_card_number }}
+    Group: {{ $group }}
+    Email: {{ $email }}
+    Phone Number: {{ $phone_number }}
+    Category: {{ $category }}
+    Registration slot: {{ $registration }}
+    Nickname: {{ $nickname }}
+    Race ID: @foreach($runNum as $number) {{ $nickname }}{{ $number->register }} @if (!$loop->last), @endif @endforeach
+
 
 Please find attached a confirmation document containing all the necessary details for your participation. It includes the event schedule, rules and regulations, and other important information. We kindly request that you review the document carefully.
 
