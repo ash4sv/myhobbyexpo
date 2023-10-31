ad6eb8213ff94fc4b1e3af44c1a1b63101b07e1b
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-01 00:35:16 +0800

    try array

--- app/Http/Controllers/Front/MHXCupController.php
+++ app/Http/Controllers/Front/MHXCupController.php
@@ -319,22 +319,22 @@ class MHXCupController extends Controller
                 Log::info('== RACER RUNNING NUMBER ==');
 
                 $billplzData = [
-                    'shopref' => $webHook['uniq'],
-                    'billplz_id' => $data['id'],
-                    'collection_id' => $data['collection_id'],
-                    'paid' => $data['paid'],
-                    'state' => $data['state'],
-                    'amount' => $data['amount'],
-                    'paid_amount' => $data['paid_amount'],
-                    'due_at' => $data['due_at'],
-                    'email' => $data['email'],
-                    'mobile' => $data['mobile'],
-                    'name' => $data['name'],
-                    'url' => $data['url'],
-                    'paid_at' => $data['paid_at'],
-                    'x_signature' => $data['x_signature'],
-                    'created_at' => now(),
-                    'updated_at' => now(),
+                    'shopref'       => $webHook['uniq'],
+                    'billplz_id'    => $data[0]['id'],
+                    'collection_id' => $data[0]['collection_id'],
+                    'paid'          => $data[0]['paid'],
+                    'state'         => $data[0]['state'],
+                    'amount'        => $data[0]['amount'],
+                    'paid_amount'   => $data[0]['paid_amount'],
+                    'due_at'        => $data[0]['due_at'],
+                    'email'         => $data[0]['email'],
+                    'mobile'        => $data[0]['mobile'],
+                    'name'          => $data[0]['name'],
+                    'url'           => $data[0]['url'],
+                    'paid_at'       => $data[0]['paid_at'],
+                    'x_signature'   => $data[0]['x_signature'],
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
                 ];
 
                 DB::table('billplz_webhook')->insert($billplzData);
