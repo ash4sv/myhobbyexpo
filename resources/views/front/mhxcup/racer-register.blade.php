ddfe21875e92e8f4435d9c5c963121e5d8cc7014
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-07 09:26:43 +0800

    update conditions

--- resources/views/front/mhxcup/racer-register.blade.php
+++ resources/views/front/mhxcup/racer-register.blade.php
@@ -82,39 +82,10 @@
 
                             <div class="mb-3">
                                 <label for="registrationSlot" class="form-label">Please select your registration slot</label>
+
                                 <select name="registration" id="registrationSlot" class="form-control">
-                                    <option value="">0</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                    <option value="6">6</option>
-                                    <option value="7">7</option>
-                                    <option value="8">8</option>
-                                    <option value="9">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                    <option value="13">13</option>
-                                    <option value="14">14</option>
-                                    <option value="15">15</option>
-                                    <option value="16">16</option>
-                                    <option value="17">17</option>
-                                    <option value="18">18</option>
-                                    <option value="19">19</option>
-                                    <option value="20">20</option>
-                                    <option value="21">21</option>
-                                    <option value="22">22</option>
-                                    <option value="23">23</option>
-                                    <option value="24">24</option>
-                                    <option value="25">25</option>
-                                    <option value="26">26</option>
-                                    <option value="27">27</option>
-                                    <option value="28">28</option>
-                                    <option value="29">29</option>
-                                    <option value="30">30</option>
                                 </select>
+
                                 <div class="invalid-feedback"></div>
                                 <div id="" class="form-text">
                                     RM{{ $category['price_category'] }} per registration
@@ -176,6 +147,68 @@
 @push('onpagescript')
     <script>
         $(document).ready(function () {
+
+            // Function to update registration options
+            function updateRegistrationOptions() {
+                var category = $("input[name='category']").val();
+                var registrationSelect = $("#registrationSlot");
+                var currentDate = new Date();
+                // Clear existing options
+                registrationSelect.empty();
+                
+                if (category === 'semi-tech class a' || category === 'b-max class b') {
+                    // Early bird registration
+                    if (currentDate <= new Date('2023-11-08')) {
+                        for (var i = 1; i <= 20; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option>');
+                        }
+                        console.log('Early bird registration only for 20');
+                    }
+                    // Pre-event online registration
+                    if (currentDate >= new Date('2023-11-09') && currentDate <= new Date('2023-12-01')) {
+                        for (var i = 1; i <= 20; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option>');
+                        }
+                        console.log('Pre-event online registration only for 15');
+                    }
+                    // Walk-in registration
+                    if (currentDate >= new Date('2023-12-02') && currentDate <= new Date('2023-12-03')) {
+                        for (var i = 1; i <= 15; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option>');
+                        }
+                        console.log('Walk-in registration only for 15');
+                    }
+                } else if (category === 'stock-car class c') {
+                    // Early bird registration
+                    if (currentDate <= new Date('2023-11-08')) {
+                        for (var i = 1; i <= 5; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option');
+                        }
+                        console.log('Early bird registration just 5')
+                    }
+                    // Pre-event online registration
+                    if (currentDate >= new Date('2023-11-09') && currentDate <= new Date('2023-12-01')) {
+                        for (var i = 1; i <= 5; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option');
+                        }
+                        console.log('Pre-event online registration just 5')
+                    }
+                    // Walk-in registration
+                    if (currentDate >= new Date('2023-12-02') && currentDate <= new Date('2023-12-03')) {
+                        for (var i = 1; i <= 5; i++) {
+                            registrationSelect.append('<option value="' + i + '">' + i + '</option');
+                        }
+                        console.log('Walk-in registration just 5')
+                    }
+                }
+            }
+
+            // Attach change event listener to the category input
+            $("input[name='category']").change(updateRegistrationOptions);
+
+            // Call the function on page load
+            updateRegistrationOptions();
+
             $("#section_c").hide();
 
             // Initialize Select2 for the initial registration slot selection
