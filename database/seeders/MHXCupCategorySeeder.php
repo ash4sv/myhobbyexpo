c66167e73ffd57639364b0cb9953a29ebafca43e
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-09 16:12:05 +0800

    update

--- database/seeders/MHXCupCategorySeeder.php
+++ database/seeders/MHXCupCategorySeeder.php
@@ -32,16 +32,18 @@ class MHXCupCategorySeeder extends Seeder
             [
                 'category' => $semitech,
                 'price_category' => $priceSemitech,
-                'total_cost' => $priceSemitech * 4,
+                'total_cost' => $priceSemitech * 2,
                 'full_name' => strtoupper('ZAINUDIN JUHARI'),
                 'identification_card_number' => '850323015487',
                 'phone_number' => '+6+60167800794',
                 'email' => 'zainudinjuhari.zj@gmail.com',
                 'nickname' => strtoupper('ARRC'),
                 'team_group' => 'Arrc',
-                'registration' => 4,
+                'registration' => 2,
                 'receipt' => '',
                 'approval' => false,
+                'payment_type' => 4,
+                'payment_status' => 0,
                 'runNum' => [
                     [
                         'category' => $semitech,
@@ -49,7 +51,7 @@ class MHXCupCategorySeeder extends Seeder
                         'uniq' => '',
                         'nickname' => strtoupper('ARRC'),
                         'register' => '',
-                        'shirt_zie' => 'l',
+                        'shirt_zie' => '',
                     ],
                     [
                         'category' => $semitech,
@@ -57,71 +59,7 @@ class MHXCupCategorySeeder extends Seeder
                         'uniq' => '',
                         'nickname' => strtoupper('ARRC'),
                         'register' => '',
-                        'shirt_zie' => 'l',
-                    ],
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'xl',
-                    ],
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'xxl',
-                    ],
-                ],
-            ],
-            [
-                'category' => $semitech,
-                'price_category' => $priceSemitech,
-                'total_cost' => $priceSemitech * 4,
-                'full_name' => strtoupper('ZAINUDIN JUHARI'),
-                'identification_card_number' => '850323015487',
-                'phone_number' => '+6+60167800794',
-                'email' => 'zainudinjuhari.zj@gmail.com',
-                'nickname' => strtoupper('ARRC'),
-                'team_group' => 'Arrc',
-                'registration' => 4,
-                'receipt' => '',
-                'approval' => false,
-                'runNum' => [
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'xl',
-                    ],
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'xl',
-                    ],
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'm',
-                    ],
-                    [
-                        'category' => $semitech,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ARRC'),
-                        'register' => '',
-                        'shirt_zie' => 'xxl',
+                        'shirt_zie' => '',
                     ],
                 ],
             ],
@@ -155,6 +93,8 @@ class MHXCupCategorySeeder extends Seeder
                 'registration'              => $racer['registration'],
                 'receipt'                   => $racer['receipt'],
                 'approval'                  => $racer['approval'],
+                'payment_type'              => $racer['payment_type'],
+                'payment_status'            => $racer['payment_status'],
             ]);
             $newRacer->save();
 
@@ -175,136 +115,136 @@ class MHXCupCategorySeeder extends Seeder
         }
 
 
-        $racerClassBs = [
-            // b-max class b
-            [
-                'category' => $bmax,
-                'price_category' => $priceBmax,
-                'total_cost' => $priceBmax * 1,
-                'full_name' => strtoupper('mohammad rozaini bin hamid'),
-                'identification_card_number' => '940908105401',
-                'phone_number' => '+60176843935',
-                'email' => 'rozainibinhamid1994@gmail.com',
-                'nickname' => strtoupper('ROZAI'),
-                'team_group' => 'ARPJ',
-                'registration' => 1,
-                'receipt' => $folder . '8989447639.jpeg',
-                'approval' => false,
-                'runNum' => [
-                    [
-                        'category' => $bmax,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('ROZAI'),
-                        'register' => '',
-                        'shirt_zie' => 'm',
-                    ],
-                ]
-            ],
-            [
-                'category' => $bmax,
-                'price_category' => $priceBmax,
-                'total_cost' => $priceBmax * 1,
-                'full_name' => strtoupper('Mohd amir bin abu bakar'),
-                'identification_card_number' => '890213145375',
-                'phone_number' => '+60122286163',
-                'email' => 'amirburn1989@gmail.com',
-                'nickname' => strtoupper('AMIRBUR'),
-                'team_group' => 'TAMIYA LONGKANG (TL)',
-                'registration' => 1,
-                'receipt' => $folder . '2425266976.png',
-                'approval' => false,
-                'runNum' => [
-                    [
-                        'category' => $bmax,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('AMIRBUR'),
-                        'register' => '',
-                        'shirt_zie' => 'l',
-                    ],
-                ]
-            ],
-            [
-                'category' => $bmax,
-                'price_category' => $priceBmax,
-                'total_cost' => $priceBmax * 2,
-                'full_name' => strtoupper('Mohd Khairul Anuar Bin Zakaria'),
-                'identification_card_number' => '860316295539',
-                'phone_number' => '+60179122784',
-                'email' => 'zizouanuar86@gmail.com',
-                'nickname' => strtoupper('NUAR'),
-                'team_group' => 'NA',
-                'registration' => 2,
-                'receipt' => $folder . '4982630673.png',
-                'approval' => false,
-                'runNum' => [
-                    [
-                        'category' => $bmax,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('NUAR'),
-                        'register' => '',
-                        'shirt_zie' => 'm',
-                    ],
-                    [
-                        'category' => $bmax,
-                        'racer_id' => '',
-                        'uniq' => '',
-                        'nickname' => strtoupper('NUAR'),
-                        'register' => '',
-                        'shirt_zie' => 'xxxxl',
-                    ],
-                ]
-            ],
-        ];
+//        $racerClassBs = [
+//            // b-max class b
+//            [
+//                'category' => $bmax,
+//                'price_category' => $priceBmax,
+//                'total_cost' => $priceBmax * 1,
+//                'full_name' => strtoupper('mohammad rozaini bin hamid'),
+//                'identification_card_number' => '940908105401',
+//                'phone_number' => '+60176843935',
+//                'email' => 'rozainibinhamid1994@gmail.com',
+//                'nickname' => strtoupper('ROZAI'),
+//                'team_group' => 'ARPJ',
+//                'registration' => 1,
+//                'receipt' => $folder . '8989447639.jpeg',
+//                'approval' => false,
+//                'runNum' => [
+//                    [
+//                        'category' => $bmax,
+//                        'racer_id' => '',
+//                        'uniq' => '',
+//                        'nickname' => strtoupper('ROZAI'),
+//                        'register' => '',
+//                        'shirt_zie' => 'm',
+//                    ],
+//                ]
+//            ],
+//            [
+//                'category' => $bmax,
+//                'price_category' => $priceBmax,
+//                'total_cost' => $priceBmax * 1,
+//                'full_name' => strtoupper('Mohd amir bin abu bakar'),
+//                'identification_card_number' => '890213145375',
+//                'phone_number' => '+60122286163',
+//                'email' => 'amirburn1989@gmail.com',
+//                'nickname' => strtoupper('AMIRBUR'),
+//                'team_group' => 'TAMIYA LONGKANG (TL)',
+//                'registration' => 1,
+//                'receipt' => $folder . '2425266976.png',
+//                'approval' => false,
+//                'runNum' => [
+//                    [
+//                        'category' => $bmax,
+//                        'racer_id' => '',
+//                        'uniq' => '',
+//                        'nickname' => strtoupper('AMIRBUR'),
+//                        'register' => '',
+//                        'shirt_zie' => 'l',
+//                    ],
+//                ]
+//            ],
+//            [
+//                'category' => $bmax,
+//                'price_category' => $priceBmax,
+//                'total_cost' => $priceBmax * 2,
+//                'full_name' => strtoupper('Mohd Khairul Anuar Bin Zakaria'),
+//                'identification_card_number' => '860316295539',
+//                'phone_number' => '+60179122784',
+//                'email' => 'zizouanuar86@gmail.com',
+//                'nickname' => strtoupper('NUAR'),
+//                'team_group' => 'NA',
+//                'registration' => 2,
+//                'receipt' => $folder . '4982630673.png',
+//                'approval' => false,
+//                'runNum' => [
+//                    [
+//                        'category' => $bmax,
+//                        'racer_id' => '',
+//                        'uniq' => '',
+//                        'nickname' => strtoupper('NUAR'),
+//                        'register' => '',
+//                        'shirt_zie' => 'm',
+//                    ],
+//                    [
+//                        'category' => $bmax,
+//                        'racer_id' => '',
+//                        'uniq' => '',
+//                        'nickname' => strtoupper('NUAR'),
+//                        'register' => '',
+//                        'shirt_zie' => 'xxxxl',
+//                    ],
+//                ]
+//            ],
+//        ];
 
-        foreach ($racerClassBs as $racer) {
-            $uniq = Str::random(4);
-
-            $registered = RacerRegister::where('category', $bmax)->orderBy('id', 'DESC')->first();
-            if ($registered) {
-                $lastNumberRegister = $registered->numberRegister->last();
-                if ($lastNumberRegister) {
-                    $register = $lastNumberRegister->register;
-                }
-            } else {
-                $register = 0; // Start from 0 if no previous records found
-            }
-
-            $newRacer = new RacerRegister();
-            $newRacer->fill([
-                'uniq'                      => $uniq,
-                'category'                  => $racer['category'],
-                'price_category'            => $racer['price_category'],
-                'total_cost'                => $racer['total_cost'],
-                'full_name'                 => $racer['full_name'],
-                'identification_card_number' => $racer['identification_card_number'],
-                'phone_number'              => $racer['phone_number'],
-                'email'                     => $racer['email'],
-                'nickname'                  => $racer['nickname'],
-                'team_group'                => $racer['team_group'],
-                'registration'              => $racer['registration'],
-                'receipt'                   => $racer['receipt'],
-                'approval'                  => $racer['approval'],
-            ]);
-            $newRacer->save();
-
-            foreach ($racer['runNum'] as $number) {
-                $nickname = new RacerNickNameRegister();
-                $nickname->fill([
-                    'category'  => $newRacer->category,
-                    'racer_id'  => $newRacer->id,
-                    'uniq'      => $newRacer->uniq,
-                    'nickname'  => $newRacer->nickname,
-                    'register'  => $register + 1,
-                    'shirt_zie' => $number['shirt_zie'],
-                ]);
-                $nickname->save();
-
-                $register = $nickname->register;
-            }
-        }
+//        foreach ($racerClassBs as $racer) {
+//            $uniq = Str::random(4);
+//
+//            $registered = RacerRegister::where('category', $bmax)->orderBy('id', 'DESC')->first();
+//            if ($registered) {
+//                $lastNumberRegister = $registered->numberRegister->last();
+//                if ($lastNumberRegister) {
+//                    $register = $lastNumberRegister->register;
+//                }
+//            } else {
+//                $register = 0; // Start from 0 if no previous records found
+//            }
+//
+//            $newRacer = new RacerRegister();
+//            $newRacer->fill([
+//                'uniq'                      => $uniq,
+//                'category'                  => $racer['category'],
+//                'price_category'            => $racer['price_category'],
+//                'total_cost'                => $racer['total_cost'],
+//                'full_name'                 => $racer['full_name'],
+//                'identification_card_number' => $racer['identification_card_number'],
+//                'phone_number'              => $racer['phone_number'],
+//                'email'                     => $racer['email'],
+//                'nickname'                  => $racer['nickname'],
+//                'team_group'                => $racer['team_group'],
+//                'registration'              => $racer['registration'],
+//                'receipt'                   => $racer['receipt'],
+//                'approval'                  => $racer['approval'],
+//            ]);
+//            $newRacer->save();
+//
+//            foreach ($racer['runNum'] as $number) {
+//                $nickname = new RacerNickNameRegister();
+//                $nickname->fill([
+//                    'category'  => $newRacer->category,
+//                    'racer_id'  => $newRacer->id,
+//                    'uniq'      => $newRacer->uniq,
+//                    'nickname'  => $newRacer->nickname,
+//                    'register'  => $register + 1,
+//                    'shirt_zie' => $number['shirt_zie'],
+//                ]);
+//                $nickname->save();
+//
+//                $register = $nickname->register;
+//            }
+//        }
 
     }
 }
