4abc4f9e41e01b2f6c6562ddf99788dd90c65a5b
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-21 13:36:27 +0800

    update email template

--- resources/views/front/participant/SendConfirmationTicketEmail.blade.php
+++ resources/views/front/participant/SendConfirmationTicketEmail.blade.php
@@ -1,12 +1,20 @@
 <x-mail::message>
-# Introduction
+# Confirmation of MHX 2023 Ticket Purchase
 
-The body of your message.
+Dear {{ json_decode($visitors->visitor)->full_name }},
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+Thank you for choosing MHX 2023! We are excited to confirm your ticket purchase for the upcoming event. Below are the details of your transaction. Kindly refer to the attached receipt for your reference.
 
-Thanks,<br>
-{{ config('app.name') }}
+Transaction Details:
+
+- Event: MHX 2023
+- Ticket Type and Quantity: @foreach(json_decode($carts->cart) as $key => $item) {{ $item->ticketType }}  {{ $item->ticketQuantity }} @if (!$loop->last), @endif @endforeach
+- Total Amount: {{ $carts->overallTotal }}
+
+We appreciate your support and look forward to welcoming you to MHX 2023. If you have any further questions or need assistance, feel free to contact our customer support team at [Customer Support Email or Phone Number].
+
+Best regards,
+
+MHX2023 Secretariat
+Sales Team <br>
 </x-mail::message>
