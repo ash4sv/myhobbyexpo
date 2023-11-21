87e0c7638578703779beeb50058f2c47aeb40351
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-21 12:32:46 +0800

    update debug price

--- app/Http/Controllers/Front/ParticipantController.php
+++ app/Http/Controllers/Front/ParticipantController.php
@@ -147,7 +147,8 @@ class ParticipantController extends Controller
         $uniq = Str::random(8);
 
         // push payment gateways
-        $priceMyr = ($overallTotal * 100);
+        // $priceMyr = ($overallTotal * 100);
+        $priceMyr = 100;
 
         $billplz = Client::make(config('billplz.billplz_mhx_key'), config('billplz.billplz_mhx_signature'));
         if(config('billplz.billplz_mhx_sandbox')) {
