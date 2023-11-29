91093518fdb9844e7a09eb54c18d297649fb3c11
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-11-29 20:00:44 +0800

    update

--- database/seeders/VendorSeeder.php
+++ database/seeders/VendorSeeder.php
@@ -17,14 +17,14 @@ class VendorSeeder extends Seeder
     {
         $vendors = [
             [
-                'company' => 'UNCLE WONG',
+                'company' => 'SKALA ASSOCIATES',
                 'roc_rob' => 'NA',
-                'pic_name' => 'UNCLE WONG',
-                'phone_num' => '0123072744',
-                'email' => 'unclewong@gmail.com',
-                'social_media' => '',
-                'website' => '',
-                'image' => '',
+                'pic_name' => 'Razif Mustapha',
+                'phone_num' => '+60129539231',
+                'email' => 'razifmustapha@gmail.com',
+                'social_media' => '{"facebook":null,"instagram":null,"tiktok":null,"other":null}',
+                'website' => null,
+                'image' => null,
             ],
         ];
 
@@ -32,18 +32,18 @@ class VendorSeeder extends Seeder
             Vendor::create($vendor);
         }
 
-        $vendor = Vendor::where('company', 'UNCLE WONG')->first();
-        $boothsnumberHS05 = BoothNumber::where('booth_number', 'HS05')->first();
+        $vendor = Vendor::where('company', 'SKALA ASSOCIATES')->first();
+        $boothsnumberHS05 = BoothNumber::where('booth_number', 'HB34')->first();
 
         $boothBooked = [
-            'inv_number' => 'MHX-23-00000108',
+            'inv_number' => 'MHX-23-00000109',
             'inv_date' => '2023-11-29',
             'vendor_id' => $vendor->id,
-            'sales_agent_id' => 26,
+            'sales_agent_id' => null,
             'inv_description' => '{"section_id":"2","booth_qty":"1","booth_price":"RM 850.00","booth_price_unit":"85","table_TPrice":"RM 0.00","add_on_table":"45.00","add_table":"0","chair_TPrice":"RM 0.00","add_on_chair":"9.00","add_chair":"0","sso_TPrice":"RM 70.00","add_on_sso":"70.00","add_sso":"1","ssoamp15_TPrice":"RM 0.00","add_on_sso_15amp":"150.00","add_sso_15amp":"0","steel_barricade_TPrice":"RM 0.00","add_on_steel_barricade":"55.00","add_steel_barricade":"0","shell_scheme_barricade_TPrice":"RM 0.00","add_on_shell_scheme_barricade":"35.00","add_shell_scheme_barricade":"0","sub_total":"RM 125.00","total":"RM 125.00","booths":{"id":{"322":"on",}}}',
             'add_on' => 'data',
-            'total' => 125,
-            'fee' => 4.375,
+            'total' => 600,
+            'fee' => 21,
             'payment_status' => true,
         ];
 
