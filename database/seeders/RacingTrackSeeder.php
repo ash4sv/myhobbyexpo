b85a77e323ba5095922c5c87e34a4cd44b75a6cd
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2023-12-01 06:26:28 +0800

    update data

--- database/seeders/RacingTrackSeeder.php
+++ database/seeders/RacingTrackSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class RacingTrackSeeder extends Seeder
 {
@@ -17,118 +18,124 @@ class RacingTrackSeeder extends Seeder
     public function run(): void
     {
         Schema::disableForeignKeyConstraints();
-        DB::table('racing_categories')->truncate();
-        DB::table('racing_tracks')->truncate();
+        // DB::table('racing_categories')->truncate();
+        // DB::table('racing_tracks')->truncate();
         DB::table('racing_racers')->truncate();
-        // DB::table('racings')->truncate();
-        // DB::table('racing_results')->truncate();
+        DB::table('racings')->truncate();
+        DB::table('racing_results')->truncate();
+        DB::table('racing_score_boards')->truncate();
         Schema::enableForeignKeyConstraints();
 
-        $path = 'assets/upload/';
-
-        $categories = [
-            [
-                'category_name' => 'Semi-Tech Class A',
-                'category_image' => $path . 'mhxc_001.png',
-            ],
-            [
-                'category_name' => 'B-Max Class B',
-                'category_image' => $path . 'mhxc_002.png',
-            ],
-            [
-                'category_name' => 'Stock-Car Class C',
-                'category_image' => $path . 'mhxc_003.png',
-            ],
-        ];
-
-        foreach ($categories as $category) {
-            RacingCategory::create($category);
-        }
-
-        $semitechclassa = RacingCategory::where('category_name', 'Semi-Tech Class A')->first();
-        $bmaxclassb = RacingCategory::where('category_name', 'B-Max Class B')->first();
-        $stockcarclassc = RacingCategory::where('category_name', 'Stock-Car Class C')->first();
-
-        $tracks = [
-            [
-                'racing_categories_id' => $semitechclassa->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-semi-tech.jpeg',
-            ],
-            [
-                'racing_categories_id' => $semitechclassa->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-semi-tech.jpeg',
-            ],
-            [
-                'racing_categories_id' => $semitechclassa->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-semi-tech.jpeg',
-            ],
-            [
-                'racing_categories_id' => $semitechclassa->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-semi-tech.jpeg',
-            ],
-
-            [
-                'racing_categories_id' => $bmaxclassb->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-b-max.jpeg',
-            ],
-            [
-                'racing_categories_id' => $bmaxclassb->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-b-max.jpeg',
-            ],
-            [
-                'racing_categories_id' => $bmaxclassb->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-b-max.jpeg',
-            ],
-            [
-                'racing_categories_id' => $bmaxclassb->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-b-max.jpeg',
-            ],
-
-            [
-                'racing_categories_id' => $stockcarclassc->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-stock.jpeg',
-            ],
-            [
-                'racing_categories_id' => $stockcarclassc->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-stock.jpeg',
-            ],
-            [
-                'racing_categories_id' => $stockcarclassc->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-stock.jpeg',
-            ],
-            [
-                'racing_categories_id' => $stockcarclassc->id,
-                'track_name' => '',
-                'track_layouts' => $path . 'layout-stock.jpeg',
-            ],
-        ];
-
-        $counters = []; // This array will store counts for each category
-
-        foreach ($tracks as $track) {
-            $categoryId = $track['racing_categories_id'];
-
-            if (!isset($counters[$categoryId])) {
-                $counters[$categoryId] = 1;
-            }
-
-            RacingTrack::create([
-                'racing_categories_id' => $track['racing_categories_id'],
-                'track_name' => 'Layout ' . $counters[$categoryId],
-                'track_layouts' => $track['track_layouts']
-            ]);
-            $counters[$categoryId]++;
-        }
+//        $path = 'assets/upload/';
+//
+//        $categories = [
+//            [
+//                'category_name' => 'Semi-Tech Class A',
+//                'category_image' => $path . 'mhxc_001.png',
+//            ],
+//            [
+//                'category_name' => 'B-Max Class B',
+//                'category_image' => $path . 'mhxc_002.png',
+//            ],
+//            [
+//                'category_name' => 'Stock-Car Class C',
+//                'category_image' => $path . 'mhxc_003.png',
+//            ],
+//        ];
+//
+//        foreach ($categories as $category) {
+//            RacingCategory::create([
+//                'slug' => Str::slug($category['category_name']),
+//                'category_name' => $category['category_name'],
+//                'category_image' => $category['category_image']
+//            ]);
+//        }
+//
+//        $semitechclassa = RacingCategory::where('category_name', 'Semi-Tech Class A')->first();
+//        $bmaxclassb = RacingCategory::where('category_name', 'B-Max Class B')->first();
+//        $stockcarclassc = RacingCategory::where('category_name', 'Stock-Car Class C')->first();
+//
+//        $tracks = [
+//            [
+//                'racing_categories_id' => $semitechclassa->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-semi-tech.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $semitechclassa->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-semi-tech.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $semitechclassa->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-semi-tech.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $semitechclassa->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-semi-tech.jpeg',
+//            ],
+//
+//            [
+//                'racing_categories_id' => $bmaxclassb->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-b-max.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $bmaxclassb->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-b-max.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $bmaxclassb->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-b-max.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $bmaxclassb->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-b-max.jpeg',
+//            ],
+//
+//            [
+//                'racing_categories_id' => $stockcarclassc->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-stock.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $stockcarclassc->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-stock.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $stockcarclassc->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-stock.jpeg',
+//            ],
+//            [
+//                'racing_categories_id' => $stockcarclassc->id,
+//                'track_name' => '',
+//                'track_layouts' => $path . 'layout-stock.jpeg',
+//            ],
+//        ];
+//
+//        $counters = []; // This array will store counts for each category
+//
+//        foreach ($tracks as $track) {
+//            $categoryId = $track['racing_categories_id'];
+//
+//            if (!isset($counters[$categoryId])) {
+//                $counters[$categoryId] = 1;
+//            }
+//
+//            RacingTrack::create([
+//                'slug' => Str::slug('Layout ' . $counters[$categoryId]),
+//                'racing_categories_id' => $track['racing_categories_id'],
+//                'track_name' => 'Layout ' . $counters[$categoryId],
+//                'track_layouts' => $track['track_layouts']
+//            ]);
+//            $counters[$categoryId]++;
+//        }
     }
 }
